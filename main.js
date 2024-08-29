3dbc5978bb5817e41ddfac559cb7da4c87f49331
Parth Panchal <panchalparth658@gmail.com>
2024-08-29 16:26:10 -0400

    Fixed api call

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function getMovie() {
 	let movieId = sessionStorage.getItem('movieId');
 
 	axios
-		.get('http://www.omdbapi.com/?apikey=92239507&i=' + movieId)
+		.get('https://www.omdbapi.com/?apikey=92239507&i=' + movieId)
 		.then((response) => {
 			console.log(response);
 			let movie = response.data;
